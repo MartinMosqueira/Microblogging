522cae80bfbc22dd4bacb358b7455bbd1a42305d
MartinMosqueira <tinchomosqueira@gmail.com>
2024-05-02 11:02:24 -0300

    fix(Test): Update cypress test

--- src/test/javascript/cypress/e2e/myTest/mensaje_muro.cy.ts
+++ src/test/javascript/cypress/e2e/myTest/mensaje_muro.cy.ts
@@ -7,10 +7,10 @@ describe('mensaje muro', () => {
     cy.get('#entity-menu__BV_toggle_ > .navbar-dropdown-menu > .no-bold').click();
     cy.get('div > :nth-child(2) > .dropdown-item').click();
     cy.get('[data-cy="entityCreateButton"] > span').click();
-    cy.get('[data-cy="mensaje"]').type('Hello world!!');
-    //cy.get('[data-cy="fecha"]').focus().type('2021-09-01')
+    cy.get('[data-cy="mensaje"]').type('Mensaje creado desde Cypress!!');
+    cy.get('[data-cy="fecha"]').type('2024-05-02T08:30');
     cy.get('[data-cy="tags"]').type('tag1');
-    cy.get('[data-cy="user"]').select('1');
+    cy.get('[data-cy="user"]').select('1051');
     cy.get('[data-cy="entityCreateSaveButton"] > span').click();
   });
 
@@ -19,7 +19,7 @@ describe('mensaje muro', () => {
     cy.login('user', 'user');
     cy.get('#entity-menu__BV_toggle_ > .navbar-dropdown-menu > .no-bold').click();
     cy.get('div > :nth-child(2) > .dropdown-item').click();
-    cy.get(':nth-child(11) > .text-right > .btn-group > [data-cy="entityEditButton"]').click();
+    cy.get('table tr:first-of-type .text-right > .btn-group > [data-cy="entityEditButton"]').click();
     cy.get('[data-cy="mensaje"]').clear().type('Hello world Update!!');
     cy.get('[data-cy="entityCreateSaveButton"] > span').click();
   });
@@ -29,7 +29,7 @@ describe('mensaje muro', () => {
     cy.login('user', 'user');
     cy.get('#entity-menu__BV_toggle_ > .navbar-dropdown-menu > .no-bold').click();
     cy.get('div > :nth-child(2) > .dropdown-item').click();
-    cy.get(':nth-child(11) > .text-right > .btn-group > [data-cy="entityDeleteButton"]').click();
+    cy.get('table tr:first-of-type .text-right > .btn-group > [data-cy="entityDeleteButton"]').click();
     cy.get('[data-cy="entityConfirmDeleteButton"]').click();
   });
 });
